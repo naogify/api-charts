95408673f409e49d3e1e19445d594cf129e61e7c
naoki <n.globe.us@gmail.com>
2020-03-10 01:45:51 +0900

    add fetch callback

--- index.php
+++ index.php
@@ -24,7 +24,41 @@ function register_myguten_block() {
 			'default' => '',
 		],],
 		'render_callback' => function ( $attributes ) {
-			return "<div>Hello</div>";
+			// $base_url = 'https://qiita.com';
+			// $tag = 'PHP';
+			// $query = ['page'=>'1','per_page'=>'5'];
+			// $response = file_get_contents(
+            //       $base_url.'/api/v2/tags/'.$tag.'/items?' .
+            //       http_build_query($query)
+			// );
+			// // https://qiita.com/api/v2/tags/PHP/items?page=1&per_page=5
+			// // 結果はjson形式で返されるので
+
+			$base_url = 'https://catalog.data.metro.tokyo.lg.jp';
+			$tag = 'package_show';
+			$query = ['id'=>'t000010d0000000068'];
+			$response = file_get_contents(
+                  $base_url.'/api/3/action/'.$tag.'?' .
+                  http_build_query($query)
+			);
+			$result = json_decode($response,true);
+			$return ="";
+			if($result["success"]){
+				$resources_url = $result["result"]["resources"][0]["url"];
+				$row = 1;
+				if (($handle = fopen($resources_url, "r")) !== FALSE) {
+					while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+						$num = count($data);
+						$return .= "<p> $num fields in line $row: <br /></p>\n";
+						$row++;
+						for ($c=0; $c < $num; $c++) {
+							$return .= $data[$c] . "<br />\n";
+						}
+					}
+					fclose($handle);
+				}
+			}
+			return $return;
 		},
 	) );
 }
