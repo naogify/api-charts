93c1256563f5f323f1f725b700ca0ea6ff19bdc1
naoki <n.globe.us@gmail.com>
2020-03-29 19:19:46 +0900

    Fix some

--- src/blocks/default/edit.js
+++ src/blocks/default/edit.js
@@ -1,15 +1,5 @@
-import { Fragment } from "@wordpress/element";
-import apiFetch from "@wordpress/api-fetch";
-import { ServerSideRender } from "@wordpress/components";
 import { drawCharts } from "./drawCharts";
-
 export const edit = props => {
-  const { attributes } = props;
   drawCharts();
-  return (
-    <Fragment>
-      <div id="chart_div">hey</div>
-      {/* <ServerSideRender block="api-charts/default" attributes={attributes} /> */}
-    </Fragment>
-  );
+  return <div id="chart_div"></div>;
 };
